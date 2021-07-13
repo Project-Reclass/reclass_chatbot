18a9c8a6fb2ec5c0461f53ecb998eed882881b10
Josiah Aviles <josiaha@uchicago.edu>
2021-07-13 15:24:53 -0500

    Cleaned up code, condensed date formatting, added two tests in main_test.go

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 )
 
@@ -11,8 +12,16 @@ func TestOutput(t *testing.T) {
 	MainOutput(buf)
 
 	got := buf.String()
-	want := "Hello World!\n"
+	want := fmt.Sprintf("{%q:null,%q:true}\n", "error", "status")
 	if got != want {
 		t.Fatalf("Got: %s - Want: %s\n", got, want)
 	}
+
+	prevId, err := GetLatestId()
+	if err != nil {
+		t.Fatalf(err.Error())
+	} else if prevId < 0 {
+		t.Fatalf("Invalid: GetLatestId() returns a negative ID")
+	}
+
 }
