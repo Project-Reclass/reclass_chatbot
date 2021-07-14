083c77ebf5e5c1906e84f054e503dcfc9f1fcfbd
Josiah Aviles <josiaha@uchicago.edu>
2021-07-14 12:44:39 -0500

    pulled logic into helper functions, added a test

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+//Start here: Do unit testing by outsourcing the logic and testing
+//those helper functions individually. E.g. do post and get, then
+//pass the result to helper functions. Then test those helper functions
+//with sample get and post results boi.
+
 import (
 	"bytes"
 	"encoding/json"
@@ -19,18 +24,7 @@ type Text struct {
 }
 
 // Return the greatest existing post ID in API, regardless of order
-func GetLatestId() (int, error) {
-	// Use the imported net/http to 'get' request and read from the API
-	botAPI, err := http.Get("http://192.168.49.2:30660/api/messages")
-	if err != nil {
-		return 0, err
-	}
-	defer botAPI.Body.Close()
-	body, err := ioutil.ReadAll(botAPI.Body)
-	if err != nil {
-		return 0, err
-	}
-
+func GetLatestId(body []byte) (int, error) {
 	// Unmarshal the body string into structs using Cont
 	var tex []Text
 	if marshalError := json.Unmarshal(body, &tex); marshalError != nil {
@@ -48,30 +42,49 @@ func GetLatestId() (int, error) {
 	return lastId, nil
 }
 
-func MainOutput(out io.Writer) {
-
+// Given the greatest existing ID in the API, this creates json for a new post
+func CreatePost(prevId int) ([]byte, error) {
 	//Creating properly formatted time
 	current := time.Now()
 	finalDate := current.Format("Mon Jan 2 2006 15:04:05 GMT-0700 (MST)")
 
-	// Putting together post via Text struct
-	prevId, err := GetLatestId()
-	if err != nil {
-		panic(err)
-	}
 	text := Text{prevId + 1, finalDate, "Reclass Bot", finalDate}
 
 	// Marshal the string to json
 	jsonReq, err := json.Marshal(text)
 	if err != nil {
+		return nil, err
+	}
+	return jsonReq, nil
+}
+
+func MainOutput(out io.Writer) {
+	// Use the imported net/http to 'get' request and read from the API
+	botAPI, err := http.Get("http://192.168.49.2:30660/api/messages")
+	if err != nil {
 		panic(err)
 	}
-
+	defer botAPI.Body.Close()
+	body, err := ioutil.ReadAll(botAPI.Body)
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Println(string(body))
+	prevId, err := GetLatestId(body)
+	if err != nil {
+		panic(err)
+	}
+	// Create the post with the correct ID
+	jsonpost, err := CreatePost(prevId)
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Println(string(jsonpost))
 	// POST the json to the API
 	resp, err := http.Post(
 		"http://192.168.49.2:30660/api/messages",
 		"application/json; charset=utf-8",
-		bytes.NewBuffer(jsonReq))
+		bytes.NewBuffer(jsonpost))
 	if err != nil {
 		panic(err)
 	}
--- main_test.go
+++ main_test.go
@@ -1,27 +1,51 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
+	"encoding/json"
 	"testing"
 )
 
 func TestOutput(t *testing.T) {
-	buf := &bytes.Buffer{}
+	/*
+		buf := &bytes.Buffer{}
 
-	MainOutput(buf)
+		MainOutput(buf)
 
-	got := buf.String()
-	want := fmt.Sprintf("{%q:null,%q:true}\n", "error", "status")
-	if got != want {
-		t.Fatalf("Got: %s - Want: %s\n", got, want)
+		got := buf.String()
+		want := fmt.Sprintf("{%q:null,%q:true}\n", "error", "status")
+		if got != want {
+			t.Fatalf("Got: %s - Want: %s\n", got, want)
+		}
+
+		prevId, err := GetLatestId()
+		if err != nil {
+			t.Fatalf(err.Error())
+		} else if prevId < 0 {
+			t.Fatalf("Invalid: GetLatestId() returns a negative ID")
+		}
+	*/
+
+	// test
+	// GetLatestId(botAPI []byte]) (int, error)
+	// CreatePost(prevId int) ([]byte, error)
+
+	// Check if CreatePost creates post with correct ID
+	post, err := CreatePost(10)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var tex Text
+	if marshalError := json.Unmarshal(post, &tex); marshalError != nil {
+		t.Fatal(marshalError)
+	} else if tex.UserID != 11 {
+		t.Fatalf("Post ID should be 11, param + 1, instead %d,", tex.UserID)
 	}
 
-	prevId, err := GetLatestId()
+	lastId, err := GetLatestId(post)
 	if err != nil {
-		t.Fatalf(err.Error())
-	} else if prevId < 0 {
-		t.Fatalf("Invalid: GetLatestId() returns a negative ID")
+		t.Fatal(err)
+	} else if lastId != 11 {
+		t.Fatalf("Last ID should be 11, is %d", lastId)
 	}
 
 }
